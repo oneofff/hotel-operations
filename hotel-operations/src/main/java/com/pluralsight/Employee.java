1dafe0a279df898d18b85954b7e4b1c497fc016e
Stanley Karol <stanislove.korol@gmail.com>
2025-06-09 09:08:57 -0400

    exercise 5

--- hotel-operations/src/main/java/com/pluralsight/Employee.java
+++ hotel-operations/src/main/java/com/pluralsight/Employee.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
@@ -35,6 +34,7 @@ public class Employee {
     public double getTotalPay() {
         return getRegularHours() * payRate +
                 getOvertimeHours() * payRate * OVERTIME_MULTIPLIER;
+
     }
 
     public void punchTimeCard(LocalTime startTime, LocalTime endTime) {
